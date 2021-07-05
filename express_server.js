70787b48b7e332f227d25161b3e8711d67ee293a
NHadjiraIA <bkh.hadjira@gmail.com>
2021-07-05 13:50:52 -0400

    URL Shortening (Part 1)

--- express_server.js
+++ express_server.js
@@ -7,7 +7,15 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+function randomString(length, chars) {
+  var result = '';
+  for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+}
+var rString = randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+console.log(rString);
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -20,7 +28,9 @@ app.get("/urls", (req, res) => {
   //pass data to the template
   res.render("urls_index", templateVars);
 });
-
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase.b2xVn2 };
   res.render("urls_show", templateVars);
@@ -28,6 +38,11 @@ app.get("/urls/:shortURL", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
